46387f127db0452914ccca734f3707bb337b6fae
Abhishek Mukati <abhishekmukati2017@gmail.com>
2022-11-09 17:20:24 +0530

    first commit

---
+++ test.rb
@@ -0,0 +1,15 @@
+ p "Hello wold!" 
+ a = 10 ; 
+ b = 12;
+ c = a +b ; 
+              puts c ; 
+
+#  print <<`EOC`
+#     echo   This is the first way of creating
+#      echo  here document ie. multiple line string.
+ 
+
+print <<`EOC1`                 # execute commands
+	echo hi there
+	echo lo there
+EOC
